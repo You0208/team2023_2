3da2c0099921db63cfcab827e80510e32bcaef5c
mae0913 <2220158@ecc.ac.jp>
2023-07-10 19:47:54 +0900

    追加スコア透過

--- SceneClear.h
+++ SceneClear.h
@@ -76,7 +76,7 @@ private:
     DirectX::XMFLOAT2 p_pos = { PointPositionX,PointPositionY };// ポイント表示位置
     int addPoint = 0;                                           // 追加ポイント
     DirectX::XMFLOAT2 ap_pos{ PointPositionX,PointPositionY };  // 追加ポイント表示位置
-    DirectX::XMFLOAT4 ap_color{ 0.741f, 0.435f, 0.435f, 1.0f };;       // 追加ポイント表示位置
+    DirectX::XMFLOAT4 ap_color{ 0.741f, 0.435f, 0.435f, 0.0f };;       // 追加ポイント表示位置
     int addPointPerformState = AddPointPerformState::begin;
 
     float Theta = 0.0f;
